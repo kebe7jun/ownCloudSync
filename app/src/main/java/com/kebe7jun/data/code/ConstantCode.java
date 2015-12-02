68a9c3bbc96b98de240333c73074d0257a5ebb1f
KEBE <kebe0412@hotmail.com>
2015-12-03 02:38:59 +0800

    Init app.

---
+++ app/src/main/java/com/kebe7jun/data/code/ConstantCode.java
@@ -0,0 +1,21 @@
+package com.kebe7jun.data.code;
+
+/**
+ * Created by kebe on 15-12-2.
+ */
+public class ConstantCode {
+
+    /**
+     * Setting value
+     */
+    public static String cloudUrl = "https://blog.kebe7jun.com";
+    public static String userName = "";
+    public static String password = "";
+    public static String photoUrl = "";
+
+    /**
+     * Only sync photo in wlan network.
+     */
+    public static boolean isOnlySyncInWlan = true;
+
+}
---
+++ app/src/main/java/com/kebe7jun/data/ui/WebViewFragment.java
@@ -0,0 +1,10 @@
+package com.kebe7jun.data.ui;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by kebe on 15-12-3.
+ */
+public class WebViewFragment extends FragmentActivity {
+
+}
